eae34f4d46649628d17eb84c209e64c46edcc005
Antonio <antoniodudas911@gmail.com>
2024-10-16 15:28:25 +0100

    added multiple tests, new logic on comments model, refactored the logic for the other tests, new controller to add comments by id, added new error catcher with a new error code in app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const { getTopics } = require("./controllers/topics.controller")
 const { getApi } = require("./controllers/endpoints.controller")
 const { getArticleById, getArticles } = require("./controllers/articles.controller")
-const { getCommentsByArticleId } = require("./controllers/comments.controller")
+const { getCommentsByArticleId, addCommentsByArticleId } = require("./controllers/comments.controller")
+
+app.use(express.json())
 
 app.get("/api", getApi);
 
@@ -15,6 +17,7 @@ app.get("/api/articles", getArticles)
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
+app.post("/api/articles/:article_id/comments", addCommentsByArticleId)
 
 
 app.use((err, request, response, next) => {
@@ -27,6 +30,13 @@ app.use((err, request, response, next) => {
 app.use((err, request, response, next) => {
   if (err.status && err.msg) {
     return response.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+});
+
+app.use((err, request, response, next) => {
+  if (err.code === "23503") {
+   return response.status(404).send({ msg: "Not found" });
   }
   next(err);
 });
--- models/comments.model.js
+++ models/comments.model.js
@@ -18,3 +18,17 @@ exports.getComments = (articleId) => {
       return comments;
     });
 };
+exports.addComments = (body, author, article_id) => {
+  return db
+    .query(
+      `
+    INSERT INTO comments (body, author, article_id) 
+    VALUES ($1, $2, $3)
+    RETURNING *;
+    `,
+      [body, author, article_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
