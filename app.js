eabf134e8c411bbdaed151f7f62c2b975e3a2075
Antonio <antoniodudas911@gmail.com>
2024-11-04 09:52:33 +0000

    added cors, allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const { getTopics } = require("./controllers/topics.controller")
 const { getApi } = require("./controllers/endpoints.controller")
@@ -6,7 +7,9 @@ const { getArticleById, getArticles, updateArticles } = require("./controllers/a
 const { getCommentsByArticleId, addCommentsByArticleId, deleteCommentById} = require("./controllers/comments.controller")
 const { getUsers } = require("./controllers/users.controller")
 
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/api", getApi);
 
